c0d457e2f64dddb5d5aa0df144a5e4e6043fb8d5
Garret Wilson <60952801+gwilson93@users.noreply.github.com>
2023-10-23 17:14:58 -0400

    Creation of basic math functions and initialization of operator/operand variables

---
+++ app.js
@@ -0,0 +1,22 @@
+// Calculator Functions
+
+function add (a, b) {
+  return +a + +b;
+}
+
+function subtract (a, b) {
+  return a - b;
+}
+
+function multiply (a, b) {
+  return a * b;
+}
+
+function divide (a,b) {
+  return a / b;
+}
+
+// Initialization of Variables
+let num1;
+let operator;
+let num2;
