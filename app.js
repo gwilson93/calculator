6fd7545472c30d72945df0e96c0e6983f35825c6
Garret Wilson <60952801+gwilson93@users.noreply.github.com>
2023-10-27 18:45:46 -0400

    appendNumber function created under refactor

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
 // Initialization of Variables
+let firstOperand = "";
+let operator = null;
+let secondOperand = "";
 
+// Add Event Listeners
+const numberbtns = document.querySelectorAll(".num");
+const operatorbtns = document.querySelectorAll(".operator");
+const equalbtn = document.querySelector(".equals");
+const deletebtn = document.querySelector(".delete");
+const clearbtn = document.querySelector(".clear");
+const currentDisplay = document.querySelector(".currentoperand");
+const lastDisplay = document.querySelector(".lastoperand");
+
+numberbtns.forEach((button) => {
+  button.addEventListener("click", () => appendNumber(button.textContent));
+})
+
+// Add Helper Functions
+function appendNumber (number) {
+  currentDisplay.textContent += number;
+}
 
 // Calculator Functions
-
 function add (a, b) {
   return +a + +b;
 }
